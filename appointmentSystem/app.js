f95ba21cd53c00ea26e2de837105dd5f2153b459
Jimmy Tran <tranj46@gmail.com>
2020-02-26 15:23:18 -0600

    Add MongoDB connection credentials

--- appointmentSystem/app.js
+++ appointmentSystem/app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// mongoDB
+mongoose.connect('mongodb+srv://itmd567:itmd567@567websystems-qgpxm.azure.mongodb.net/test?retryWrites=true&w=majority', 
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error: ', err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
