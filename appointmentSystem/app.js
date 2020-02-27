797d9191b874282abe79397d6ee0d45a45241f75
dkomarov <dkomarov@hawk.iit.edu>
2020-02-27 00:10:14 -0600

    install nodemon & create env vars

--- appointmentSystem/app.js
+++ appointmentSystem/app.js
@@ -11,11 +11,11 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // mongoDB
-mongoose.connect('mongodb+srv://itmd567:itmd567@567websystems-qgpxm.azure.mongodb.net/test?retryWrites=true&w=majority', 
-    {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    }
+mongoose.connect('mongodb+srv://itmd567:'+process.env.MONGODB_PW+'@567websystems-qgpxm.azure.mongodb.net/test?retryWrites=true&w=majority', 
+  {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+  }  
 )
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error: ', err));
