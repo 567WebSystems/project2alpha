d718ae921ded55ca2927a9dadabf786890b2cdef
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-02-29 19:30:20 -0600

    create keys.js file to store google Oauth credentials

--- appointmentSystem/config/passport-setup.js
+++ appointmentSystem/config/passport-setup.js
@@ -1,11 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-
+const keys = require('./keys');
 passport.use(
     new GoogleStrategy({
     //options gor the google strategies
-    clientID: '1012259665491-h9mjitf64jabj5b5fm03umu5c6a84t9o.apps.googleusercontent.com',
-    clientSecret: 'bZtqSBXzK7MtfNHrSOXya8Us'
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
     }), ()=> {
         //passport callback function
     }
