1016e04999e324afc808c644a64f4753785c42e3
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-03-04 08:28:36 -0600

    save user information into databse
    It also prevents from saving user information multiple times

--- appointmentSystem/config/passport-setup.js
+++ appointmentSystem/config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
 passport.use(
     new GoogleStrategy({
     //options gor the google strategies
@@ -10,5 +11,23 @@ passport.use(
     }, (accessToken, refreshToken, profile, done)=> {
         //passport callback function
         console.log(profile);
+
+        //check if user already exists in database
+        User.findOne({
+            googleId: profile.id
+        }).then((currentUser) => {
+            if(currentUser){
+                //User exists
+                console.log('user already exists', currentUser);
+            }else{
+                //create User
+                new User({
+                    userName: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('new user was created' +newUser);
+                });
+            }
+        });
     })
 )
--- appointmentSystem/models/user-model.js
+++ appointmentSystem/models/user-model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //user schema to store the user information in the mongoDB
-const userSchema = new schema({
-    username: String,
-    googleID: String,
+const userSchema = new Schema({
+    userName: String,
+    googleId: String,
 });
 
 const User = mongoose.model('user', userSchema);
