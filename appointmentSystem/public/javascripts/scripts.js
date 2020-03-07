80869edd7820ae85adab538dc4267c5dbf12bbe9
dkomarov <dkomarov@hawk.iit.edu>
2020-03-07 14:28:41 -0600

    correct paths to public scripts from views

--- appointmentSystem/public/javascripts/scripts.js
+++ appointmentSystem/public/javascripts/scripts.js
@@ -1,22 +1,28 @@
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function deleteEvent() {
-    return gapi.client.calendar.calendarList.delete({})
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-            },
-            function(err) { console.error("Execute error", err); });
-} 
+console.log("scripts.js loaded")
 
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "1012259665491-h9mjitf64jabj5b5fm03umu5c6a84t9o.apps.googleusercontent.com"});
-  });
-
-function listEvents() {
-    return gapi.client.calendar.calendarList.list({})
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
+function HelloWorld() {
+  console.log("Hello World");
 }
+
+// // Make sure the client is loaded and sign-in is complete before calling this method.
+// function deleteEvent() {
+//     return gapi.client.calendar.calendarList.delete({})
+//         .then(function(response) {
+//                 // Handle the results here (response.result has the parsed body).
+//                 console.log("Response", response);
+//             },
+//             function(err) { console.error("Execute error", err); });
+// } 
+
+//   gapi.load("client:auth2", function() {
+//     gapi.auth2.init({client_id: "1012259665491-h9mjitf64jabj5b5fm03umu5c6a84t9o.apps.googleusercontent.com"});
+//   });
+
+// function listEvents() {
+//     return gapi.client.calendar.calendarList.list({})
+//         .then(function(response) {
+//                 // Handle the results here (response.result has the parsed body).
+//                 console.log("Response", response);
+//               },
+//               function(err) { console.error("Execute error", err); });
+// }
