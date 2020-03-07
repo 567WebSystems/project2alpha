e4e78ae6f737cc761bbfa16e679e7fc896cf0b79
Jimmy Tran <tranj46@gmail.com>
2020-03-06 20:12:32 -0600

    Add index.js to script.js and migrate functions to script.js

--- appointmentSystem/public/javascripts/script.js
+++ appointmentSystem/public/javascripts/script.js
@@ -1,4 +1,4 @@
-//const index = require("../../routes/index");
+const index = require("../../routes/index");
 //import { HelloWorld } from "../../routes/index";
 //const jQuery = require("https://code.jquery.com/jquery-3.4.1.min.js");
 
@@ -193,12 +193,42 @@
                 },
                 function(err) { console.error("Execute error", err); });
     }
+
     gapi.load("client:auth2", function() {
       gapi.auth2.init({client_id: "1012259665491-h9mjitf64jabj5b5fm03umu5c6a84t9o.apps.googleusercontent.com"});
     });
 
     
     }
+      //const {google} = require('googleapis');
+      //const calendar = google.calendar({version: 'v3', auth});
+
+      function onSignIn(googleUser) {
+
+        //console.log("Calendar ID: " + calendarList.list())
+
+        console.log("Welcome! You are now signed in.");
+
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        $("#appointmentForm").css("display", "block");
+      }
+
+      function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
+
+        $("#appointmentForm").css("display", "none");
+      }
   }
 //});
     
