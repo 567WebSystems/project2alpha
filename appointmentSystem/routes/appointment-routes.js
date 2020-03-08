e3dfcf2adb52307c5ee120e34c79365c17c6542c
dkomarov <dkomarov@hawk.iit.edu>
2020-03-07 18:26:13 -0600

    restore my code — was deleted without permission

--- appointmentSystem/routes/appointment-routes.js
+++ appointmentSystem/routes/appointment-routes.js
@@ -93,12 +93,13 @@ router.post("/", function(req, res){
   });
 
 router.delete('/',authCheck,(req,res)=>{
-  return gapi.client.calendar.calendarList.delete({})
-  .then(function(response) {
-          // Handle the results here (response.result has the parsed body).
-          console.log("Response", response);
-      },
-      function(err) { console.error("Execute error", err); });
+  console.log("On delete route...")
+  // return gapi.client.calendar.calendarList.delete({})
+  // .then(function(response) {
+  //         // Handle the results here (response.result has the parsed body).
+  //         console.log("Response", response);
+  //     },
+  //     function(err) { console.error("Execute error", err); });
 });
 
 module.exports = router;
--- appointmentSystem/routes/index.js
+++ appointmentSystem/routes/index.js
@@ -1,9 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+
+const gCalendar = require('../controllers/gCalendar');
+const Event = require('../models/event_model');
+
+var calendarData = {};
+var startDateObj = "text";
+var endDateObj = "text";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index');
 });
+
+router.post("/", function(req, res){
+    let rb = req.body;
+  
+    startDateObj = new Date(rb.startDate + " " + rb.startTime);
+    endDateObj = new Date(rb.endDate + " " + rb.endTime);
+
+  
+    console.log("startDateObj is: " + startDateObj);
+    console.log("endDateObj is: " + endDateObj);
+  
+    const event = new Event({ // parse event
+      _id: mongoose.Types.ObjectId(),
+      summary: rb.summary,
+      location: rb.location,
+      description: rb.description,
+      start: startDateObj,
+      end: endDateObj,
+      recurrence: rb.recurrence,
+      attendees: rb.attendees,
+      reminders: rb.reminders,
+    });
+  
+    console.log("event is: " + event)
+    console.log("Attempting to store in db...")
+    return event.save() // store event in db
+  
+    .then(result => {
+      console.log(result); // display stored event
+      res.render('appointment')
+  
+      var status = {
+        message: 'Event stored to DB.',
+      }
+  
+     console.log("status: " + status.message)
+  
+   calendarData = {
+      _id: mongoose.Types.ObjectId(),
+      'summary': rb.summary,
+      'location': rb.location,
+      'description': rb.description,
+      'start': startDateObj,
+      'end': endDateObj,
+      'recurrence': rb.recurrence,
+      'attendees': rb.attendees,
+      'reminders': rb.reminders
+   }
+  
+    console.log("calendarData is: ", calendarData);
+    gCalendar(calendarData);
+    //functions.googleCal;
+    
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+          error: err
+      });
+    });
+  
+    
+  });
 
 module.exports = router;
