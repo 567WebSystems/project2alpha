40343774863cdc62aff7f217acedc4b4bbbea830
Jimmy Tran <tranj46@gmail.com>
2020-03-07 21:38:18 -0600

    Resolve merge conflicts

--- appointmentSystem/controllers/gCalendar.js
+++ appointmentSystem/controllers/gCalendar.js
@@ -1,74 +1,3 @@
-<<<<<<< HEAD:appointmentSystem/public/javascripts/script.js
-  // var calendarData = {
-  //       'summary': index.param.event.summary,
-  //       'location': index.param.event.location,
-  //       'description': index.param.event.description,
-  //       'start': index.param.startDateObj,
-  //       'end': index.param.endDateObj,
-  //       'recurrence': index.param.event.recurrence,
-  //       'attendees': index.param.event.attendees,
-  //       'reminders': index.param.event.reminders
-  //    }
-
-  //console.log("scriptJS event is: ", calendarData)
-    
-    // Make sure the client is loaded and sign-in is complete before calling this method.
-    function deleteEvent() {
-      return gapi.client.calendar.calendarList.delete({})
-          .then(function(response) {
-                  // Handle the results here (response.result has the parsed body).
-                  console.log("Response", response);
-                },
-                function(err) { console.error("Execute error", err); });
-    }
-    
-    function listAllEvents() {
-      return gapi.client.calendar.calendarList.list({})
-          .then(function(response) {
-                  // Handle the results here (response.result has the parsed body).
-                  console.log("Response", response);
-                },
-                function(err) { console.error("Execute error", err); });
-    }
-
-    gapi.load("client:auth2", function() {
-      gapi.auth2.init({client_id: "1012259665491-h9mjitf64jabj5b5fm03umu5c6a84t9o.apps.googleusercontent.com"});
-    });
-
-      //const {google} = require('googleapis');
-      //const calendar = google.calendar({version: 'v3', auth});
-
-      function onSignIn(googleUser) {
-
-        //console.log("Calendar ID: " + calendarList.list())
-
-        console.log("Welcome! You are now signed in.");
-
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
-
-        $("#appointmentForm").css("display", "block");
-      }
-
-      function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-          console.log('User signed out.');
-        });
-
-        $("#appointmentForm").css("display", "none");
-      }
-//});
-    
-=======
-const appRoutes = require('../routes/appointment-routes');
-
 function gCal(calendarData) {
     if (calendarData) {
       const fs = require('fs');
@@ -220,4 +149,3 @@ function gCal(calendarData) {
   }
 
   module.exports = gCal;
->>>>>>> 44b680116b219700100495e3ee484d726a348964:appointmentSystem/controllers/gCalendar.js
--- appointmentSystem/routes/index.js
+++ appointmentSystem/routes/index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-const functions = require('../controller/functions');
-=======
->>>>>>> 44b680116b219700100495e3ee484d726a348964
-
 
 const gCalendar = require('../controllers/gCalendar');
 const Event = require('../models/event_model');
@@ -20,7 +15,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/", function(req, res){
-<<<<<<< HEAD
   let rb = req.body;
 
   startDateObj = new Date(rb.startTime + rb.startDate);
@@ -83,13 +77,7 @@ router.post("/", function(req, res){
   }
 
   console.log(calendarData);
-  functions.gCal(calendarData);
-=======
-    let rb = req.body;
-  
-    startDateObj = new Date(rb.startDate + " " + rb.startTime);
-    endDateObj = new Date(rb.endDate + " " + rb.endTime);
->>>>>>> 44b680116b219700100495e3ee484d726a348964
+  gCalendar(calendarData);
 
   
     console.log("startDateObj is: " + startDateObj);
@@ -106,53 +94,7 @@ router.post("/", function(req, res){
       attendees: rb.attendees,
       reminders: rb.reminders,
     });
-<<<<<<< HEAD
   });
 });
 
-module.exports = {router, functions};
-=======
-  
-    console.log("event is: " + event)
-    console.log("Attempting to store in db...")
-    return event.save() // store event in db
-  
-    .then(result => {
-      console.log(result); // display stored event
-      res.render('appointment')
-  
-      var status = {
-        message: 'Event stored to DB.',
-      }
-  
-     console.log("status: " + status.message)
-  
-   calendarData = {
-      _id: mongoose.Types.ObjectId(),
-      'summary': rb.summary,
-      'location': rb.location,
-      'description': rb.description,
-      'start': startDateObj,
-      'end': endDateObj,
-      'recurrence': rb.recurrence,
-      'attendees': rb.attendees,
-      'reminders': rb.reminders
-   }
-  
-    console.log("calendarData is: ", calendarData);
-    gCalendar(calendarData);
-    //functions.googleCal;
-    
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-          error: err
-      });
-    });
-  
-    
-  });
-
 module.exports = router;
->>>>>>> 44b680116b219700100495e3ee484d726a348964
