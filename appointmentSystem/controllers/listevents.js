705aa063169b45c372a43d7369f6800d83e9270e
Jimmy Tran <tranj46@gmail.com>
2020-03-09 14:56:52 -0500

    Minor change to ejs and add listevents.js

---
+++ appointmentSystem/controllers/listevents.js
@@ -0,0 +1,27 @@
+const {google} = require('googleapis');
+
+function listEvents(auth) {
+    const calendar = google.calendar({version: 'v3', auth});
+    //console.log("calendarList is: " + calendar.calendarList.list);
+    calendar.events.list({
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime',
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const events = res.data.items;
+      if (events.length) {
+        console.log('Upcoming 10 events:');
+        events.map((event, i) => {
+          const start = event.start.dateTime || event.start.date;
+          console.log(`${start} - ${event.summary}`);
+        });
+      } else {
+        console.log('No upcoming events found.');
+      }
+    });
+  }
+
+module.exports = listEvents;
