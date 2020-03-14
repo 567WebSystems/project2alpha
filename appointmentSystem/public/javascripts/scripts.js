671d32d28cf81844cb8024c2c95203dfc1e44352
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-03-14 13:33:25 -0500

    remove unused code

--- appointmentSystem/public/javascripts/scripts.js
+++ appointmentSystem/public/javascripts/scripts.js
@@ -65,37 +65,12 @@ function formValidation(){
   return true;
 }
 
-// Make sure the client is loaded and sign-in is complete before calling this method.
-function deleteEvent() {
-    return gapi.client.calendar.calendarList.delete({})
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-            },
-            function(err) { console.error("Execute error", err); });
-} 
-
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "1012259665491-h9mjitf64jabj5b5fm03umu5c6a84t9o.apps.googleusercontent.com"});
-  });
-
-function listEvents() {
-    return gapi.client.calendar.calendarList.list({})
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-}
-
 function del(eventID,eventSummary){
   if(confirm("Confirm do you want to delete "+ eventSummary +" event?")){
     $.ajax({url: 'http://localhost:3000/appointment/view-appointment', 
     data: {de:eventID},
     type: "POST",
     success:function(res){
-      window.location.reload(true);
     }});
   }
 }
-
