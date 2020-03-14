5c7851ac826c4d3075b8285f1056024da0c20c9e
dkomarov <dkomarov@hawk.iit.edu>
2020-03-14 18:45:15 -0500

    resolve remaining merge conflicts

--- appointmentSystem/routes/appointment-routes.js
+++ appointmentSystem/routes/appointment-routes.js
@@ -1,23 +1,11 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-
-const gcalFunction = require('../controllers/gcalendar');
-=======
 const gcalFunction = require('../controllers/gCalendar');
->>>>>>> 1c6d4e1c50679d0607d3acc2ba59f8ac90b95d4a
 const Event = require('../models/event_model');
 
 var calendarData = {};
-<<<<<<< HEAD
-var startDateObj = "text";
-var endDateObj = "text";
-
-
-=======
 var startDateObj;
 var endDateObj;
->>>>>>> 1c6d4e1c50679d0607d3acc2ba59f8ac90b95d4a
 const authCheck = (req,res, next) =>{
     if(!req.user){
         // if user is not logged in
@@ -59,40 +47,6 @@ router.post("/view-appointment",authCheck,(req,res)=>{
 router.post("/", function(req, res){
     let rb = req.body;
   
-<<<<<<< HEAD
-    startDateObj = new Date(rb.startDate + " " + rb.startTime);
-    endDateObj = new Date(rb.endDate + " " + rb.endTime);
-
-  
-    //console.log("startDateObj is: " + startDateObj);
-    //console.log("endDateObj is: " + endDateObj);
-  
-    const event = new Event({ // parse event
-      _id: mongoose.Types.ObjectId(),
-      summary: rb.summary,
-      location: rb.location,
-      description: rb.description,
-      start: startDateObj,
-      end: endDateObj,
-      recurrence: rb.recurrence,
-      attendees: rb.attendees,
-      reminders: rb.reminders,
-    });
-  
-    console.log("event is: " + event)
-    console.log("Attempting to store in db...")
-    return event.save() // store event in db
-  
-    .then(result => {
-      console.log(result); // display stored event
-     // res.status(201).json({
-      var status = {
-        message: 'Event stored to DB.',
-      }
-  
-     console.log("status: " + status.message)
-  
-=======
     startDateObj = new Date(rb.startTime +" "+ rb.startDate);
     endDateObj = new Date(rb.endTime +" "+ rb.endDate);
   
@@ -102,7 +56,6 @@ router.post("/", function(req, res){
     console.log("startDateObj is: " + startDateObj);
     console.log("endDateObj is: " + endDateObj);
 
->>>>>>> 1c6d4e1c50679d0607d3acc2ba59f8ac90b95d4a
    calendarData = {
       _id: mongoose.Types.ObjectId(),
       'summary': rb.summary,
